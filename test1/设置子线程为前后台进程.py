cad0d3904e6660197fc8f38bf5b89db51068e794
Tomzem <Tomzem@github.com>
2018-07-12 10:02:32 +0800

    python线程学习demo

--- test1/设置子线程为前后台进程.py
+++ test1/设置子线程为前后台进程.py
@@ -12,7 +12,7 @@ for i in range(2):
     t = Thread(target=do_thread, args=(i,))
     # 不设置 就是前台进程 主线程等子线程执行完才可以结束
     # 设置为TRUE 则为后天进程  主线程直接结束
-    # t.setDaemon(True)
+    t.setDaemon(True)
     t.start()
     t.setName("{0}".format(str(i)))
     print(t.getName())
